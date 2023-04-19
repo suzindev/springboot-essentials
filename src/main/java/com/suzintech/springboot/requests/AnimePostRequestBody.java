faea977f5ef12d0725980503e55a12df46c05a4b
Fernando Suzin <suzin.dev@gmail.com>
2023-04-19 20:15:49 -0300

    Finalizado implementação de testes

--- src/main/java/com/suzintech/springboot/requests/AnimePostRequestBody.java
+++ src/main/java/com/suzintech/springboot/requests/AnimePostRequestBody.java
@@ -1,11 +1,15 @@
 package com.suzintech.springboot.requests;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AnimePostRequestBody {
 
     @NotEmpty(message = "The anime name cannot be empty")
